889c33c5a780296a218fa35bc45b47a0095e86ae
Ricdelo <riccardo.giacomo18@gmail.com>
2023-11-14 15:15:58 +0100

    classe Carta

--- Server(Java)/src/Carta.java
+++ Server(Java)/src/Carta.java
@@ -1,3 +1,45 @@
+import org.w3c.dom.Element;
+
 public class Carta{
+
+    private String seed;
+    private char number;
+    private int value;
+    private String img_path;
     
+    public Carta(String seed, char number, int value, String img_path) {
+        this.seed = seed;
+        this.number = number;
+        this.value = value;
+        this.img_path = img_path;
+    }
+    
+    //costruttore parser XML
+    public Carta(Element e){
+        this.seed = XMLserializer.parseTagName(e, "seed");
+        this.number = XMLserializer.parseTagName(e, "number");
+        this.value = Integer.parseInt(XMLserializer.parseTagName(e, "value"));
+        this.img_path = XMLserializer.parseTagName(e, "img_path");
+    }
+
+    public String getSeed() {
+        return seed;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getImg_path() {
+        return img_path;
+    }
+
+    
+
+
+
 }
