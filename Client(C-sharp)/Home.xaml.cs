8fb67661bab89bc75504820fd5ebc1a9e8749d94
YasserKhaloufi <yasserkhaloufi2@gmail.com>
2023-11-20 17:51:53 +0100

    Client: implementato passaggio home-->mainWindow

--- Client(C-sharp)/Home.xaml.cs
+++ Client(C-sharp)/Home.xaml.cs
@@ -19,9 +19,14 @@ namespace Client_C_sharp_
     /// </summary>
     public partial class Home : Window
     {
+        // Questa finestra si aprirà prima della main window
         public Home()
         {
             InitializeComponent();
+
+            this.Background = new SolidColorBrush(Color.FromRgb(0, 255, 0)); // Setto lo sfondo (giusto per provare)
+
+            txtNome.Text = "Giovanni"; // Per debug
         }
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
@@ -32,6 +37,8 @@ namespace Client_C_sharp_
 
                 // Da qui in poi dovrei aspettare di ricevere la lista di room esistenti
                 MessageBox.Show(Server.Receive());
+
+                this.Close(); // Passo alla main window
             }
         }
         private void btnImpostazioni_Click(object sender, RoutedEventArgs e)
@@ -45,7 +52,14 @@ namespace Client_C_sharp_
             this.Show(); // La rivisualizzo
 
             if (imp.ipAndPort != null) //Controllo nel caso chiudessi la finestra con la x
-                Server.handShake(imp.ipAndPort.ElementAt(0), int.Parse(imp.ipAndPort.ElementAt(1)));
+            {
+                String indirizzo = imp.ipAndPort.ElementAt(0); int porta = int.Parse(imp.ipAndPort.ElementAt(1));
+
+                if (!Server.isSameSrv(indirizzo, porta))
+                    Server.handShake(indirizzo, porta);
+                else
+                    MessageBox.Show("Il server indicato è già connesso");
+            }
         }
 
         // Roba di grafica
--- Client(C-sharp)/MainWindow.xaml.cs
+++ Client(C-sharp)/MainWindow.xaml.cs
@@ -25,59 +25,17 @@ namespace Client_C_sharp_
         {
             // Impostazioni finestra
             InitializeComponent();
-            this.Background = new SolidColorBrush(Color.FromRgb(0, 255, 0)); // Setto lo sfondo (giusto per provare)
 
-            txtNome.Text = "Giovanni"; // Per debug
-
-        }
-
-        private void btnStart_Click(object sender, RoutedEventArgs e)
-        {
-            if (txtNome.Text != "" && txtNome.Text != "Inserisci nome:")
-            {
-                String messaggio = "<Username>"+txtNome.Text+"</Username>\n";
-                Server.Send(messaggio);
-                
-                // Da qui in poi dovrei aspettare di ricevere la lista di room esistenti
-                MessageBox.Show(Server.Receive());
-            }
-        }
-        private void btnImpostazioni_Click(object sender, RoutedEventArgs e)
-        {
-            Impostazioni imp = new Impostazioni();
+            // Mostro la finestra iniziale
+            Home home = new Home();
 
             this.Hide(); // Nascondo la finestra principale
 
-            imp.ShowDialog(); //...
+            home.ShowDialog(); //...
 
             this.Show(); // La rivisualizzo
 
-            if (imp.ipAndPort != null) //Controllo nel caso chiudessi la finestra con la x
-            {
-                String indirizzo = imp.ipAndPort.ElementAt(0); int porta = int.Parse(imp.ipAndPort.ElementAt(1));
 
-                if (!Server.isSameSrv(indirizzo, porta))
-                    Server.handShake(indirizzo, porta);
-                else
-                    MessageBox.Show("Il server indicato è già connesso");
-            }
-        }
-
-        // Roba di grafica
-        private void txtNome_GotFocus(object sender, RoutedEventArgs e)
-        {
-            if (txtNome.Text == "Inserisci nome:")
-            {
-                txtNome.Text = string.Empty;
-            }
-        }
-
-        private void txtNome_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(txtNome.Text))
-            {
-                txtNome.Text = "Inserisci nome:";
-            }
         }
     }
 }
